ea2705af92e5ccda6ffc11382fc96b0e3841e97e
syahfei-venturo <nur.syahfei@gmail.com>
2026-06-09 16:33:32 +0700

    sync: iOS source to 1.2.4 (telemetry hit-and-forget, UUID scanner_id, 5-type ScanType, web bundle)

--- Sources/Scanner/ScanType.swift
+++ Sources/Scanner/ScanType.swift
@@ -1,6 +1,14 @@
 import Foundation
 
 public enum ScanType: Int, Sendable {
+    /// QR codes (QR, Micro QR).
     case qr = 0
+    /// Linear 1D barcodes (EAN, UPC, Code128, Code39, Codabar, ITF).
     case barcode = 1
+    /// PDF417 stacked linear barcodes.
+    case pdf417 = 2
+    /// Aztec 2D codes.
+    case aztec = 3
+    /// Data Matrix 2D codes.
+    case dataMatrix = 4
 }
